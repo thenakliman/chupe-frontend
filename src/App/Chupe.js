ef55e79bd94ba9b8843591d3a8eb6135a223e0d4
Rajiv Kumar <thenakliman@gmail.com>
2018-04-21 20:25:28 +0530

    [thenakliman] #1 Added empty page for the Chupe project

---
+++ src/App/Chupe.js
@@ -0,0 +1,16 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+
+require('./chupe.css');
+
+/**
+* Root component of the application.
+*
+* @author: thenakliman
+* @param {Object} props, properties for the component.
+* @return {Chupe} the root component for the application.
+*/
+function Chupe(props) {
+  return <div> <header cl assName="Header"> Welcome to Chupe</header> </div>;
+}
+
+export default Chupe;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+import ReactDOM from 'react-dom';
+import Chupe from './App/Chupe'; // eslint-disable-line no-unused-vars
+
+ReactDOM.render(<Chupe />, document.getElementById('root'));
