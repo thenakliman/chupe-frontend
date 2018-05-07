2be0658e9d8317df21c7270fc7935126b96497ed
Rajiv Kumar <thenakliman@gmail.com>
2018-05-07 08:52:11 +0530

    [thenakliman] #14 Added form data to be stored in component state

--- src/App/Components/Result/QuestionResult.js
+++ src/App/Components/Result/QuestionResult.js
@@ -1,29 +1,41 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import propTypes from 'prop-types';
 
+require('./QuestionResult.css');
+
+
 /** QuestionResult component with input field having for asking
 * question.
 */
 export class QuestionResult extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {question: 'add your question here'};
+
+        this.handleChange = this.handleChange.bind(this);
+    }
     /** Get all questions on component mount */
     componentDidMount() {
         // TODO(thenaliman): Uncomment it once backend api has been implemented
         // this.props.getQuestions();
+    }
+
+    handleChange(event) {
+        this.setState({question: event.target.value});
     }
     /** Returns input field in form with a button.
     *
     * @return {object} Component for asking question.
     */
     render() {
-        console.log('in question result component');
         return (
             <div id='ask-question-div' className='input-field-for-question'>
-                <form id="ask-question-form">
+                <form id="ask-question-form" onSubmit = {this.props.askQuestion}>
                     <input type="text"
-                           id="ask-question-input-field"/>
-                    <button type="button"
-                            onClick={this.props.askQuestion}>
-                                    Ask Question
+                        id="ask-question-input-field"
+                        value={this.state.value} />
+                    <button type="button" value="Submit">
+                        Ask Question
                     </button>
                 </form>
             </div>
