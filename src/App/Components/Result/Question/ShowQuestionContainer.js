5e8205453bc5b15b87efdebcf55140520152a1d0
Rajiv Kumar <thenakliman@gmail.com>
2018-05-30 20:41:00 +0530

    [thenakliman] #37 Fixed lint error

--- src/App/Components/Result/Question/ShowQuestionContainer.js
+++ src/App/Components/Result/Question/ShowQuestionContainer.js
@@ -8,7 +8,7 @@ import {connect} from 'react-redux';
 */
 function mapStateToProps(state) {
     const currentQuestion = state.questions.questionsData.find(
-        question => question.id === state.currentView.currentQuestion);
+        (question) => question.id === state.currentView.currentQuestion);
     return {
       id: currentQuestion.id,
       question: currentQuestion.question,
