85d476a00a512c6756f7db84e73a260d23e0c52c
Rajiv Kumar <thenakliman@gmail.com>
2018-06-20 20:17:27 +0530

    [thenakliman] #26 Allowed edition of description and question field

--- src/App/Components/Result/Question/QuestionAnswerWrapper.js
+++ src/App/Components/Result/Question/QuestionAnswerWrapper.js
@@ -4,14 +4,36 @@ require('./QuestionAnswerWrapper.css');
 
 /** Question answer header component. */
 export class QuestionAnswerWrapper extends React.Component {
+  /** Constructor for the QuestionAnswerWrapper.
+   *  @param {object} props, properties of the component
+   */
   constructor(props) {
     super(props);
     this.toggleEditingMode = this.toggleEditingMode.bind(this);
-    this.state = {isEditingHeader: false};
+    this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+    this.state = {isEditingHeader: false, bodyText: props.bodyText};
   }
 
+  // todo(thenakliman): optimization here by using some alternate approach.
+  /** Called on update of the component.
+   * @param {object} prevProps, previous property of the component
+   */
+  componentDidUpdate(prevProps) {
+    if (this.props.bodyText !== prevProps.bodyText) {
+      this.setState({bodyText: this.props.bodyText});
+    }
+  }
+
+  /** Toggle editing mode for the component. */
   toggleEditingMode() {
     this.setState({isEditingHeader: !this.state.isEditingHeader});
+  }
+
+  /** Handle change in description.
+   * @param {object} event caused by changed in description field
+   */
+  handleDescriptionChange(event) {
+    this.setState({bodyText: event.target.value});
   }
   /** Provide rendering for the Question Answer
    * @return {object} returns QuestionAnswerWrapper
@@ -29,7 +51,7 @@ export class QuestionAnswerWrapper extends React.Component {
                 <span id='question-answer-wrapper-table-header-text-id'>
                     {this.props.headerText}
                 </span>
-                <span id='edit-question-answer-wrapper-content-id'
+                <span id='edit-question-answer-wrapper-id'
                       className='edit-button'
                       onClick={this.toggleEditingMode}
                 >
@@ -42,7 +64,12 @@ export class QuestionAnswerWrapper extends React.Component {
             <tr className='table-border'>
               <td className='table-border'
                   id='question-answer-wrapper-table-body-id'>
-                {this.props.bodyText}
+                {this.state.isEditingHeader?
+                  <textarea id="question-answer-wrapper-table-textarea-id"
+                    className="description-textarea"
+                    value={this.state.bodyText}
+                    onChange={this.handleDescriptionChange}/>
+                  :this.state.bodyText}
               </td>
             </tr>
           </tbody>
--- src/App/Components/Result/Question/QuestionAnswerWrapper.spec.js
+++ src/App/Components/Result/Question/QuestionAnswerWrapper.spec.js
@@ -24,22 +24,60 @@ describe('should have a box', () => {
 
   it('should have an edit text on the right', () => {
     const wrapper = shallow(<QuestionAnswerWrapper headerText='' bodyText=''/>)
-        .find('#edit-question-answer-wrapper-content-id')
+        .find('#edit-question-answer-wrapper-id');
 
     expect(wrapper.length).toEqual(1);
   });
 
   it('should change text from Edit to Save on click', () => {
-    const wrapper = shallow(<QuestionAnswerWrapper headerText='' bodyText=''/>)
+    const wrapper = shallow(<QuestionAnswerWrapper headerText='' bodyText=''/>);
     expect(
-      wrapper.find('#edit-question-answer-wrapper-content-id')
+      wrapper.find('#edit-question-answer-wrapper-id')
         .get(0).props.children).toEqual('Edit');
 
-    wrapper.find('#edit-question-answer-wrapper-content-id').simulate('click');
+    wrapper.find('#edit-question-answer-wrapper-id').simulate('click');
 
     expect(
-      wrapper.find('#edit-question-answer-wrapper-content-id')
+      wrapper.find('#edit-question-answer-wrapper-id')
         .get(0).props.children).toEqual('Save');
+  });
+
+  it('should show text area if clicked on edit button', () => {
+    const wrapper = shallow(<QuestionAnswerWrapper headerText='' bodyText=''/>);
+    wrapper.find('#edit-question-answer-wrapper-id').simulate('click');
+    const event = {target: {value: 'test value'}};
+    wrapper.find('#question-answer-wrapper-table-textarea-id')
+      .simulate('change', event);
+
+    expect(
+      wrapper.find('#question-answer-wrapper-table-textarea-id').length
+    ).toEqual(1);
+  });
+
+  it('should show have initial text as per bodyText', () => {
+    const bodyText = 'body text';
+    const wrapper = shallow(
+      <QuestionAnswerWrapper headerText='' bodyText={bodyText}/>);
+
+    expect(
+      wrapper.find('#question-answer-wrapper-table-body-id')
+        .get(0).props.children).toEqual(bodyText);
+  });
+
+  it('should save data to text area on click of save', () => {
+    const wrapper = shallow(
+      <QuestionAnswerWrapper headerText='' bodyText=''/>);
+
+    const bodyText = 'test value';
+    wrapper.setState({isEditingHeader: true, bodyText: 'body text'});
+    const event = {target: {value: bodyText}};
+    wrapper.find('#question-answer-wrapper-table-textarea-id')
+      .simulate('change', event);
+
+    wrapper.find('#edit-question-answer-wrapper-id').simulate('click');
+    expect(
+      wrapper.find('#question-answer-wrapper-table-body-id')
+        .get(0).props.children).toEqual(bodyText);
   });
 
   it('should show text in header of table from props', () => {
