3367fc54b7d8cc730ddb2919127011946e4448d0
Rajiv Kumar <thenakliman@gmail.com>
2018-06-30 14:03:41 +0530

    [thenakliman] #40 Added cookies utility for storing and fetching

---
+++ src/App/utils/cookies.js
@@ -0,0 +1,55 @@
+import Cookies from 'universal-cookie';
+import {verify, decode} from 'jsonwebtoken';
+
+const TOKEN_COOKIE_KEY = 'token';
+const TOKEN_EXPIRY_COOKIE_KEY = 'expiryTime';
+const TOKEN_USERNAME_KEY = 'username';
+const audience = 'chupe-frontend';
+const issuer = 'chupe';
+
+export let cookies = new Cookies();
+
+const setCookie = (key, value) => {
+  cookies.set(key, value);
+};
+
+export const setCookies = (token) => {
+  verify(token,
+         'chupe-key',
+         {
+            algorithms: ['HS512'],
+            audience: audience,
+            issuer: issuer,
+            ignoreExpiration: false,
+         },
+         (e) => console.log(e)
+  );
+
+  const decodedToken = decode(token);
+  setToken(token);
+  setTokenExpiryTime(decodedToken.exp);
+  setUsername(decodedToken.username);
+};
+
+const setToken = (token) => {
+  setCookie(TOKEN_COOKIE_KEY, token);
+};
+
+const setTokenExpiryTime = (expiryTime) => {
+  setCookie(TOKEN_EXPIRY_COOKIE_KEY, expiryTime);
+};
+
+const setUsername = (userName) => {
+  setCookie(TOKEN_USERNAME_KEY, userName);
+};
+
+export const getToken = () => {
+  if (!cookies.get(TOKEN_COOKIE_KEY) ||
+      !cookies.get(TOKEN_EXPIRY_COOKIE_KEY) ||
+      cookies.get(TOKEN_EXPIRY_COOKIE_KEY) < Date.now()) {
+    return null;
+    console.log(cookies.get(TOKEN_EXPIRY_COOKIE_KEY), Date.now());
+  }
+
+  return cookies.get(TOKEN_COOKIE_KEY);
+};
---
+++ src/App/utils/cookies.spec.js
@@ -0,0 +1,51 @@
+import * as cookies from './cookies';
+
+describe('getCookie', () => {
+  afterEach(()=>{
+    cookies.cookies.remove('token');
+    cookies.cookies.remove('expiryTime');
+  });
+
+  it('should return null when get cookie is called', () => {
+    expect(cookies.getToken()).toEqual(null);
+  });
+
+  it('should return null if expiry time is not set', () => {
+    cookies.cookies.set('token', null);
+    expect(cookies.getToken()).toEqual(null);
+  });
+
+  it('should return token if expiry time is greater than current time', () => {
+    const token = 'testToken';
+    cookies.cookies.set('token', token);
+    // todo(thenakliman) hack, use something better to set date of previous day
+    cookies.cookies.set('expiryTime', Date.now()+10000);
+    expect(cookies.getToken()).toEqual(token);
+  });
+
+  it('should return null if expiry time is less than current time', () => {
+    cookies.cookies.set('token', 'TestToken');
+    cookies.cookies.set('expiryTime', Date.now()-10);
+    expect(cookies.getToken()).toEqual(null);
+  });
+});
+
+describe('set cookies', () => {
+  /* eslint-disable */
+  const token = 'eyJhbGciOiJIUzUxMiJ9.eyJhdWQiOiJjaHVwZS1mcm9udGVuZCIsInN1YiI6InByYXN1bl9oYXphcmkiLCJyb2xlcyI6WyJhZG1pbiJdLCJuYW1lIjoiUHJhc3VuIGhhemFyaSIsImlzcyI6ImNodXBlIiwiZXhwIjoxNTMwMzM2NDA0LCJpYXQiOjE1MzAzMzU4MDQsImVtYWlsIjoicGhhemFyaUBleGFtcGxlLmNvbSIsInVzZXJuYW1lIjoicHJhc3VuX2hhemFyaSJ9.jQrBmj8X9sN41zMyUFjB3dy53uASBravnsjNgahh09-HbHrdqu5MF2sBxL2YSluVJqRtR6SGzIcWrfuDNqIVJw';
+  /* eslint-enable */
+  it('should set token cookies', () => {
+    cookies.setCookies(token);
+    expect(cookies.cookies.get('token')).toEqual(token);
+  });
+
+  it('should set username cookies', () => {
+    cookies.setCookies(token);
+    expect(cookies.cookies.get('username')).toEqual('prasun_hazari');
+  });
+
+  it('should set expiryTime cookies', () => {
+    cookies.setCookies(token);
+    expect(cookies.cookies.get('expiryTime')).toEqual('1530336404');
+  });
+});
