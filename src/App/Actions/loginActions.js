01aafbcf53c668bc34b90a9c7921dc5b29a7749c
Rajiv Kumar <thenakliman@gmail.com>
2018-06-30 17:05:29 +0530

    [thenakliman] #40 Added an action for getting token

--- src/App/Actions/loginActions.js
+++ src/App/Actions/loginActions.js
@@ -1,8 +1,18 @@
 import {ActionTypes} from './ActionTypes';
+import * as TokenService from '../Services/TokenService';
 
 export const setUsername = (username) => {
   return {
     type: ActionTypes.SET_USERNAME,
     payload: username,
   };
+};
+
+export const authenticate = (username) => async (dispatch) => {
+  const token = await TokenService.getToken(username);
+  if (!token) {
+    return false;
+  }
+  dispatch(setUsername(username));
+  return true;
 };
--- src/App/Actions/loginActions.spec.js
+++ src/App/Actions/loginActions.spec.js
@@ -1,11 +1,38 @@
-import {setUsername} from './loginActions';
+import {authenticate} from './loginActions';
 import {ActionTypes} from './ActionTypes';
+import * as TokenService from '../Services/TokenService';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+
 describe('Login Actions', () => {
-  it('should create an action with given username', () => {
-      const username = 'test-username';
-      expect(setUsername(username)).toEqual({
+  let store;
+  beforeEach(() => {
+    const middleware = [thunk];
+    store = configureStore(middleware)({});
+    store.clearActions();
+  });
+
+  it('should call question service and dispatch set username', async () => {
+    const username = 'test-user';
+    spyOn(TokenService, 'getToken').and.returnValue('fakeToken');
+
+    await store.dispatch(authenticate(username));
+
+    expect(TokenService.getToken).toHaveBeenCalledWith(username);
+    expect(store.getActions()).toEqual([{
         payload: username,
         type: ActionTypes.SET_USERNAME,
-      });
+    }]);
+  });
+
+  it('should call question service but not dispatch any action', async () => {
+    const username = 'test-user';
+    spyOn(TokenService, 'getToken').and.returnValue(null);
+
+    await store.dispatch(authenticate(username));
+
+    expect(TokenService.getToken).toHaveBeenCalledWith(username);
+    expect(store.getActions()).toEqual([]);
   });
 });
