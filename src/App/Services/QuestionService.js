8c93303c1f8e60c2e15f8a1d356369d3e395dc95
Rajiv Kumar <thenakliman@gmail.com>
2018-07-22 14:04:22 +0530

    [thenakliman] #15 Use client in question service

--- src/App/Services/QuestionService.js
+++ src/App/Services/QuestionService.js
@@ -1,41 +1,17 @@
-const axios = require('axios');
+import {get, put, post} from './client';
 
-const CHUPE_QUESTION_URL='/api/v1';
+const CHUPE_QUESTION_URL='/api/v1/question';
 
 export const QuestionService = {
   getQuestions() {
-      return axios({
-        method: 'get',
-        url: `${CHUPE_QUESTION_URL}/question`,
-        headers: {'Content-Type': 'application/json'},
-      })
-      .then((response)=>response.data)
-      .catch((error) => {
-          console.log(error.message);
-      });
+      return get(CHUPE_QUESTION_URL);
   },
+
   askQuestion(question) {
-      return axios({
-        method: 'post',
-        url: `${CHUPE_QUESTION_URL}/question`,
-        headers: {'Content-Type': 'application/json'},
-        data: question,
-      })
-      .then((response)=>response)
-      .catch((error) => {
-          console.log(error.message);
-      });
+      return post(CHUPE_QUESTION_URL, question);
   },
+
   updateQuestion(question) {
-      return axios({
-        method: 'put',
-        url: `${CHUPE_QUESTION_URL}/question/${question.id}`,
-        headers: {'Content-Type': 'application/json'},
-        data: question,
-      })
-      .then((response)=>response)
-      .catch((error) => {
-          console.log(error.message);
-      });
+      return put(`${CHUPE_QUESTION_URL}/${question.id}`, question);
   },
 };
--- src/App/Services/QuestionService.spec.js
+++ src/App/Services/QuestionService.spec.js
@@ -1,74 +1,41 @@
-import axios from 'axios';
 import {QuestionService} from './QuestionService';
-import MockAdapter from 'axios-mock-adapter';
+import * as Client from './client';
 
 
 describe('Question Service', () => {
-    it('Returns data when get questions is called', (done) => {
-        let mock = new MockAdapter(axios);
-        const questionData = {
-            data: [{'question': 'what?'}, {'question': 'when?'}]};
+    it('Returns data when get questions is called', async () => {
+        const questions = [{'question': 'what?'}, {'question': 'when?'}];
+        spyOn(Client, 'get').and.returnValue(questions);
 
-        mock.onGet('/api/v1/question').reply(200, questionData);
+        const receivedQuestions = await QuestionService.getQuestions();
 
-        QuestionService.getQuestions(0, 'any').then((response) => {
-            expect(response.data).toEqual(questionData.data);
-            done();
-        });
+        expect(Client.get).toHaveBeenCalledWith('/api/v1/question');
+        expect(receivedQuestions).toEqual(questions);
     });
 
-    it('Response of getQuestions call fails', async () => {
-        let mock = new MockAdapter(axios);
-        mock.onGet('/api/v1/question').reply(404);
-        spyOn(console, 'log');
-        await QuestionService.getQuestions();
-        expect(console.log)
-            .toHaveBeenCalledWith('Request failed with status code 404');
+    it('Returns when ask questions is called', async () => {
+        const question = {'question': 'what?'};
+        spyOn(Client, 'post').and.returnValue(question);
+
+        const receivedQuestions = await QuestionService.askQuestion(question);
+
+        expect(Client.post).toHaveBeenCalledWith(
+            '/api/v1/question', question);
+
+        expect(receivedQuestions).toEqual(question);
     });
 
-    it('Returns when ask questions is called', (done) => {
-        let mock = new MockAdapter(axios);
-        const questionData = {data: {'question': 'what?'}};
+    it('Return 204 when update question is called', async () => {
+        const question = {'question': 'what?', 'id': 10};
+        const updatedAnswer = 'updatedAnswer';
+        spyOn(Client, 'put').and.returnValue(updatedAnswer);
 
-        mock.onPost('/api/v1/question', questionData.data)
-            .reply(200, questionData.data);
+        const receivedQuestions = await QuestionService.updateQuestion(
+            question);
 
-        QuestionService.askQuestion(questionData.data).then((response) => {
-            expect(response.data).toEqual(questionData.data);
-            done();
-        });
-    });
+        expect(Client.put).toHaveBeenCalledWith(
+            '/api/v1/question/10', question);
 
-    it('Response of askQuestions call fails', async () => {
-        let mock = new MockAdapter(axios);
-        const questionData = {data: {'question': 'what?'}};
-        mock.onPost('/api/v1/question', questionData.data).reply(404);
-        spyOn(console, 'log');
-        await QuestionService.askQuestion(questionData.data);
-        expect(console.log)
-            .toHaveBeenCalledWith('Request failed with status code 404');
-    });
-
-    it('Return 204 when update question is called', (done) => {
-        let mock = new MockAdapter(axios);
-        const questionData = {data: {question: 'what?', id: 1}};
-
-        mock.onPut('/api/v1/question/1', questionData.data)
-            .reply(204);
-
-        QuestionService.updateQuestion(questionData.data).then((response) => {
-            expect(response.data).toEqual(undefined);
-            expect(response.status).toEqual(204);
-            done();
-        });
-    });
-    it('Response of updateQuestions call fails', async () => {
-        let mock = new MockAdapter(axios);
-        const questionData = {data: {'question': 'what?', 'id': 1}};
-        mock.onPut('/api/v1/question/1', questionData.data).reply(500);
-        spyOn(console, 'log');
-        await QuestionService.updateQuestion(questionData.data);
-        expect(console.log)
-            .toHaveBeenCalledWith('Request failed with status code 500');
+        expect(receivedQuestions).toEqual(updatedAnswer);
     });
 });
