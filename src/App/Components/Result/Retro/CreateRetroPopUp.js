65a6b05d8004d556458c1a56ed275d304ebc402c
Rajiv Kumar <thenakliman@gmail.com>
2019-05-14 20:17:12 +0530

    [thenakliman] #0000 Fix styling for create retro pop up

--- src/App/Components/Result/Retro/CreateRetroPopUp.js
+++ src/App/Components/Result/Retro/CreateRetroPopUp.js
@@ -31,37 +31,39 @@ export class CreateRetroPopUp extends React.Component {
 
     render() {
       return (
-      <div id='create-retro-container-id' className='create-retro-container'>
-          <div className={'retro-button-container'}>
-              <input className={'retro-creation-input'}
-                     id={'create-retro-name'}
-                     onChange={
-                          (event) => this.handleRetroName(event.target.value)}
-                     value={this.state.name}
-              />
-              <input className={'maximum-vote-count'}
-                     id={'create-retro-maximum-vote-count'}
-                     onChange={
-                          (event) => this.handleMaximumVote(event.target.value)}
-                     value={this.state.maximumVote}
-              />
-              <div className='create-retro-buttons-container'>
-                  <button id='create-retro-button-id'
-                    className='create-retro-button'
-                    type='button'
-                    onClick={this.createRetro}
-                  >
-                    Create Retro
-                  </button>
-                  <button id='cancel-create-retro-button-id'
-                    className='create-retro-button'
-                    onClick={this.props.closeCreatePopUp}
-                    type='button'
-                  >
-                    Cancel
-                  </button>
-              </div>
-          </div>
+      <div className='retro-pop-up-container'>
+        <div id='create-retro-container-id' className='create-retro-container'>
+            <div className={'retro-button-container'}>
+                <input className={'retro-creation-input'}
+                       id={'create-retro-name'}
+                       onChange={
+                            (event) => this.handleRetroName(event.target.value)}
+                       value={this.state.name}
+                />
+                <input className={'maximum-vote-count'}
+                       id={'create-retro-maximum-vote-count'}
+                       onChange={
+                            (event) => this.handleMaximumVote(event.target.value)}
+                       value={this.state.maximumVote}
+                />
+                <div className='create-retro-buttons-container'>
+                    <button id='create-retro-button-id'
+                      className='create-retro-button'
+                      type='button'
+                      onClick={this.createRetro}
+                    >
+                      Create Retro
+                    </button>
+                    <button id='cancel-create-retro-button-id'
+                      className='create-retro-button'
+                      onClick={this.props.closeCreatePopUp}
+                      type='button'
+                    >
+                      Cancel
+                    </button>
+                </div>
+            </div>
+        </div>
       </div>);
     }
 }
