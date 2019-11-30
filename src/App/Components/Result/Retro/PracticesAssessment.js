e8f85451f60e3462c75d6b980548ed3c483d7d58
Rajiv Kumar <thenakliman@gmail.com>
2019-11-30 19:30:45 +0530

    #53 Show project assessments

---
+++ src/App/Components/Result/Retro/PracticesAssessment.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import propTypes from 'prop-types'
+
+require('./PracticesAssessment.scss')
+
+export class PracticesAssessment extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
+  componentWillMount () {
+    this.props.fetchPractices()
+  }
+
+  render () {
+    return <div class='practices'>
+             <table>
+               <tbody>
+                 {this.props.practices.map((practice, index) => (
+                    <tr>
+                      <td>
+                        <div key={index}>
+                          {practice.description}
+                        </div>
+                      </td>
+                      <td>
+                        <span>Yes</span>
+                        <span>No</span>
+                      </td>
+                    </tr>
+                  ))}
+               </tbody>
+             </table>
+           </div>
+  }
+}
+
+PracticesAssessment.propTypes = {
+  practices: propTypes.array.isRequired,
+  fetchPractices: propTypes.func.isRequired
+}
